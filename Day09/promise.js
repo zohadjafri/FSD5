71ca152b5603531fcb99528df96c93b2a40973bc
zohadjafri <zohadjafri670@gmail.com>
2025-09-30 14:58:07 +0530

    await

--- Day09/promise.js
+++ Day09/promise.js
@@ -9,14 +9,22 @@ const wait = (msg,sec)=>{
   },sec);
   });
 }
-const bookTicket=()=>{
-  printMessage("Welcome");
-  wait("Login Sucessfully",3000)
-  .then(()=>wait("seat has been selected sucessfully",2000))
-  .then(()=>wait("your seat has been booked",3000))
-  .then(()=>wait("your seat has been booked",4000))
-  .then(()=>wait("Ticket has been confirmed",3000))
-  .then(()=>printMessage("Thankyou"))
-}
+// const bookTicket=()=>{
+//     printMessage("Welcome");
+//     wait("Login Sucessfully",3000)
+//     .then(()=>wait("seat has been selected sucessfully",2000))
+//     .then(()=>wait("your seat has been booked",3000))
+//     .then(()=>wait("your seat has been booked",4000))
+//     .then(()=>wait("Ticket has been confirmed",3000))
+//     .then(()=>printMessage("Thankyou"))
+// }
 
-bookTicket();
+
+const bookTicket=async()=>{
+  printMessage("Welcome");
+  await wait("Login Sucessfully",3000);
+  await wait("seat has been selected sucessfully",4000);
+  await wait("your seat has been booked",3000);
+  await wait("Payment has been done",4000);
+  await wait("Ticket has been confirmed",3000);
+}
